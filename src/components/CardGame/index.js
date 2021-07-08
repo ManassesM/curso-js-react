91c7c441be190b09648928c80f879eb1a767d355
ManassesM <manamiui2001@gmail.com>
2021-07-08 11:38:22 -0300

    Create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+const CardGame = () => {
+    return `
+        <article class="card-game">
+            <img src="img/alura-pixel.png" alt="logo da alura">
+        </article>
+    `
+    
+}
+
+export default CardGame
